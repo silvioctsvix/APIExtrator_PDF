13e1dc1844bca8084fa10d51a3829a09329699bd
Silvio Cesar Teixeira dos Santos <154469806+silvioctsvix@users.noreply.github.com>
2024-02-12 09:22:46 -0300

    Update Extrator_PDF_API_V2.py

--- Extrator_PDF_API_V2.py
+++ Extrator_PDF_API_V2.py
@@ -4,25 +4,23 @@ import pytesseract
 from PIL import Image
 import requests
 import io
+from concurrent.futures import ThreadPoolExecutor
 import tempfile
 import os
 
 app = Flask(__name__)
 
 def baixar_arquivo_pdf(url):
-    """Baixa um arquivo PDF do URL fornecido e retorna o caminho do arquivo temporário."""
+    """Baixa um arquivo PDF do URL fornecido e retorna o objeto BytesIO."""
     resposta = requests.get(url)
     if resposta.status_code == 200:
-        # Cria um arquivo temporário para o PDF
-        arquivo_temp = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
-        arquivo_temp.write(resposta.content)
-        arquivo_temp.close()
-        return arquivo_temp.name
+        return io.BytesIO(resposta.content)
     else:
         raise Exception(f"Erro ao baixar o arquivo PDF: Status {resposta.status_code}")
 
-def extrair_texto_ocr_da_imagem(doc, pagina):
+def extrair_texto_ocr_da_imagem(doc, pagina_numero):
     texto_ocr = ''
+    pagina = doc.load_page(pagina_numero)
     imagens = pagina.get_images(full=True)
     for img_index in imagens:
         xref = img_index[0]
@@ -32,12 +30,19 @@ def extrair_texto_ocr_da_imagem(doc, pagina):
         texto_ocr += pytesseract.image_to_string(imagem)
     return texto_ocr
 
-def ocrizar_pdf(caminho_pdf):
+def processar_pagina(doc, pagina_numero):
+    texto_pagina = ''
+    pagina = doc.load_page(pagina_numero)
+    texto_pagina += pagina.get_text()
+    texto_pagina += extrair_texto_ocr_da_imagem(doc, pagina_numero)
+    return texto_pagina
+
+def ocrizar_pdf(arquivo_pdf):
     texto_ocr = ''
-    with fitz.open(caminho_pdf) as doc:
-        for pagina in doc:
-            texto_ocr += pagina.get_text()
-            texto_ocr += extrair_texto_ocr_da_imagem(doc, pagina)
+    with fitz.open(stream=arquivo_pdf, filetype="pdf") as doc:
+        with ThreadPoolExecutor() as executor:
+            resultados = executor.map(lambda pagina: processar_pagina(doc, pagina), range(len(doc)))
+            texto_ocr = ''.join(resultados)
     return texto_ocr
 
 @app.route('/extrair_texto', methods=['POST'])
@@ -48,8 +53,8 @@ def extrair_texto():
         return jsonify({"erro": "URL não fornecida"}), 400
 
     try:
-        caminho_pdf = baixar_arquivo_pdf(url_pdf)
-        texto_ocr = ocrizar_pdf(caminho_pdf)
+        arquivo_pdf = baixar_arquivo_pdf(url_pdf)
+        texto_ocr = ocrizar_pdf(arquivo_pdf)
         return jsonify({"texto": texto_ocr})
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
@@ -57,4 +62,3 @@ def extrair_texto():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    #app.run(debug=True)
