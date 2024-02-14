c982cd4ac4069d4cd0011cc5a50744a0bbde06ce
Silvio Cesar Teixeira dos Santos <154469806+silvioctsvix@users.noreply.github.com>
2024-02-14 07:34:10 -0300

    Update Extrator_PDF_API_V2.py

--- Extrator_PDF_API_V2.py
+++ Extrator_PDF_API_V2.py
@@ -1,31 +1,39 @@
 from flask import Flask, request, jsonify
 import fitz  # PyMuPDF
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 import tempfile
 import io
 
 app = Flask(__name__)
 
-# Certifique-se de configurar o caminho para o executável do Tesseract OCR, se necessário.
-# Exemplo: pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+# Função otimizada para extrair texto de páginas com imagens
 def extrair_texto_ocr_de_pagina_com_imagem(doc, pagina):
     texto_ocr = ''
     pix = pagina.get_pixmap()
-    imagem = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-    texto_ocr += pytesseract.image_to_string(imagem, lang='por')
+    imagem_original = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+    # Redimensionamento da imagem - Reduz para 1500px de largura mantendo a proporção
+    base_width = 1500
+    w_percent = (base_width / float(imagem_original.size[0]))
+    h_size = int((float(imagem_original.size[1]) * float(w_percent)))
+    imagem_redimensionada = imagem_original.resize((base_width, h_size), Image.ANTIALIAS)
+
+    # Ajuste de contraste
+    enhancer = ImageEnhance.Contrast(imagem_redimensionada)
+    imagem_contraste = enhancer.enhance(2)  # Ajuste o fator conforme necessário
+
+    texto_ocr += pytesseract.image_to_string(imagem_contraste, lang='por')
     return texto_ocr
 
 def extrair_texto_ocr_da_imagem_aperfeicoada(doc, pagina):
     texto_ocr = ''
-    # Verifica se há texto selecionável na página antes de tentar OCR nas imagens
     texto_selecionavel = pagina.get_text("text")
     if texto_selecionavel:
         return texto_selecionavel
     else:
-        # Para páginas sem texto selecionável, aplica a abordagem otimizada de OCR em toda a página
+        # Aplica a abordagem otimizada de OCR em toda a página para páginas sem texto selecionável
         return extrair_texto_ocr_de_pagina_com_imagem(doc, pagina)
 
 def ocrizar_pdf(caminho_pdf):
@@ -33,7 +41,6 @@ def ocrizar_pdf(caminho_pdf):
     try:
         with fitz.open(caminho_pdf) as doc:
             for pagina in doc:
-                # Utiliza a função aperfeiçoada para extrair texto, seja diretamente ou via OCR
                 texto_ocr += extrair_texto_ocr_da_imagem_aperfeicoada(doc, pagina)
     except Exception as e:
         return str(e)
@@ -48,14 +55,12 @@ def convert_pdf():
     if file.filename == '':
         return jsonify({"error": "Nenhum arquivo selecionado"}), 400
     
-    # Utiliza um diretório temporário para salvar o arquivo
     temp_dir = tempfile.mkdtemp()
     temp_path = os.path.join(temp_dir, file.filename)
     file.save(temp_path)
     
     texto_ocr = ocrizar_pdf(temp_path)
     
-    # Remove o arquivo temporário e o diretório
     os.remove(temp_path)
     os.rmdir(temp_dir)
     
@@ -67,4 +72,3 @@ def convert_pdf():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    #app.run(debug=True)
