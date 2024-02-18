87d2e06402ef12e4d4f04bb9e48e634d9bc39d7a
Silvio Cesar Teixeira dos Santos <154469806+silvioctsvix@users.noreply.github.com>
2024-02-18 12:11:49 -0300

    Update Extrator_PDF_API_V2.py

--- Extrator_PDF_API_V2.py
+++ Extrator_PDF_API_V2.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-import fitz  # PyMuPDF, também conhecido como fitz
+import fitz  # PyMuPDF
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageEnhance
 import logging
 import tempfile
 import os
@@ -28,8 +28,13 @@ def extrair_texto_ocr_de_pagina_com_imagem(pagina):
     texto_ocr = ''
     pix = pagina.get_pixmap()
     imagem_original = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+    # Ajuste de contraste
+    enhancer = ImageEnhance.Contrast(imagem_original)
+    imagem_contrastada = enhancer.enhance(2.0)  # O fator de contraste, >1 aumenta o contraste
+
     custom_config = '--oem 1 --psm 3'
-    texto_ocr += pytesseract.image_to_string(imagem_original, lang='por', config=custom_config)
+    texto_ocr += pytesseract.image_to_string(imagem_contrastada, lang='por', config=custom_config)
     return texto_ocr
 
 def ocrizar_pdf(caminho_pdf, paginas_param):
@@ -38,7 +43,7 @@ def ocrizar_pdf(caminho_pdf, paginas_param):
     paginas_a_processar = parse_paginas_param(paginas_param)
     with fitz.open(caminho_pdf) as doc:
         for num_pagina in paginas_a_processar:
-            if num_pagina - 1 < len(doc):  # Ajuste para indexação base-0
+            if num_pagina - 1 < len(doc):
                 pagina = doc.load_page(num_pagina - 1)
                 texto_ocr += extrair_texto_ocr_de_pagina_com_imagem(pagina)
             else:
@@ -48,7 +53,6 @@ def ocrizar_pdf(caminho_pdf, paginas_param):
 @app.route('/convert', methods=['POST'])
 def convert_pdf():
     logging.info("Iniciando conversão do PDF")
-    # Agora, 'paginas' é obtido dos parâmetros de query da URL
     paginas_param = request.args.get('paginas', '')
 
     if 'application/pdf' not in request.headers['Content-Type']:
@@ -60,10 +64,10 @@ def convert_pdf():
 
     temp_dir = tempfile.mkdtemp()
     temp_path = os.path.join(temp_dir, "uploaded_file.pdf")
-    
+
     with open(temp_path, 'wb') as f:
         f.write(file_content)
-    
+
     try:
         texto_ocr = ocrizar_pdf(temp_path, paginas_param)
     except Exception as e:
